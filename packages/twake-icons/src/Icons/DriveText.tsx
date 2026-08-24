a8a172decb642461d4cc6c666199b587c73e276b
Camille Moussu <cmoussu@linagora.com>
2026-08-24 12:30:33 +0200

    feat(twake-icons): Add DriveText for Drive logotype

---
+++ packages/twake-icons/src/Icons/DriveText.tsx
@@ -0,0 +1,93 @@
+// Automatically created, please run `scripts/generate-svgr.sh ` to regenerate;
+import * as React from "react";
+import type { SVGProps } from "react";
+function SvgDriveText(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 66 22"
+      {...props}
+    >
+      <path
+        fill="url(#drive-text_svg__a)"
+        d="M0 .642h7.3q3.444 0 5.78 1.314 2.366 1.314 3.533 3.62 1.168 2.307 1.168 5.315 0 2.978-1.168 5.284-1.167 2.307-3.533 3.62-2.336 1.285-5.78 1.285H0zm6.803 16.76q3.445 0 5.08-1.752 1.665-1.782 1.665-4.76t-1.665-4.759q-1.635-1.81-5.08-1.81H4.088v13.08z"
+      />
+      <path
+        fill="url(#drive-text_svg__b)"
+        d="M19.667 5.898h3.825v2.89q.409-1.665 1.577-2.51 1.197-.847 3.124-.73v3.708h-.555q-1.752 0-2.89 1.109-1.14 1.08-1.14 2.949v7.766h-3.94z"
+      />
+      <path
+        fill="url(#drive-text_svg__c)"
+        d="M29.504 5.898h3.942V21.08h-3.942zm-.409-3.591q0-.965.672-1.635A2.36 2.36 0 0 1 31.46 0q1.022 0 1.694.672.67.67.671 1.635 0 .992-.671 1.664-.672.672-1.694.671a2.36 2.36 0 0 1-1.693-.671 2.32 2.32 0 0 1-.672-1.664"
+      />
+      <path
+        fill="url(#drive-text_svg__d)"
+        d="M34.171 5.898h4.672l3.036 8.671q.41 1.11.643 2.424.233-1.314.642-2.424L46.2 5.898h4.584L44.478 21.08h-3.971z"
+      />
+      <path
+        fill="url(#drive-text_svg__e)"
+        d="M57.817 21.46q-2.306 0-4.088-1.051a7.6 7.6 0 0 1-2.774-2.89q-.992-1.81-.992-4a8.1 8.1 0 0 1 1.022-4 7.77 7.77 0 0 1 2.803-2.92q1.81-1.08 4.029-1.08 2.22 0 3.97 1.08a7.34 7.34 0 0 1 2.745 2.92q.993 1.81.993 4 0 .73-.117 1.4H54.05q.351 1.373 1.314 2.22.993.817 2.453.817 1.225 0 2.19-.554a4.45 4.45 0 0 0 1.518-1.49l3.066 2.307q-.934 1.46-2.745 2.365-1.81.876-4.03.876m3.737-9.49a4.27 4.27 0 0 0-1.372-2.189q-1.022-.876-2.424-.876-1.372 0-2.394.847-.993.846-1.314 2.219z"
+      />
+      <defs>
+        <linearGradient
+          id="drive-text_svg__a"
+          x1={30.217}
+          x2={30.217}
+          y1={-5.44}
+          y2={24.131}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF6372" />
+          <stop offset={1} stopColor="#FF3347" />
+        </linearGradient>
+        <linearGradient
+          id="drive-text_svg__b"
+          x1={30.217}
+          x2={30.217}
+          y1={-5.44}
+          y2={24.131}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF6372" />
+          <stop offset={1} stopColor="#FF3347" />
+        </linearGradient>
+        <linearGradient
+          id="drive-text_svg__c"
+          x1={30.217}
+          x2={30.217}
+          y1={-5.44}
+          y2={24.131}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF6372" />
+          <stop offset={1} stopColor="#FF3347" />
+        </linearGradient>
+        <linearGradient
+          id="drive-text_svg__d"
+          x1={30.217}
+          x2={30.217}
+          y1={-5.44}
+          y2={24.131}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF6372" />
+          <stop offset={1} stopColor="#FF3347" />
+        </linearGradient>
+        <linearGradient
+          id="drive-text_svg__e"
+          x1={30.217}
+          x2={30.217}
+          y1={-5.44}
+          y2={24.131}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#FF6372" />
+          <stop offset={1} stopColor="#FF3347" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+export default SvgDriveText;
+
