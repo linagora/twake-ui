969bada6b692ad75c853e516109199e717cfd092
Théo Poizat <hello@zatteo.com>
2026-09-14 15:26:25 +0200

    feat(twake-mui): Migrate Nav from cozy-ui

--- packages/twake-mui/src/index.ts
+++ packages/twake-mui/src/index.ts
@@ -20,6 +20,15 @@ export { TwakeMuiThemeProvider } from './components/ThemeProvider'
 export { Avatar, default as AvatarDefault } from './components/Avatar'
 export { default as Chip } from './components/Chip'
 export { Sidebar } from './components/Sidebar'
+export {
+  Nav,
+  NavItem,
+  NavLink,
+  NavIcon,
+  NavText,
+  NavDesktopLimiter,
+  NavDesktopDropdown
+} from './components/Nav'
 export { nameToColor, supportedColors } from './components/Avatar/helpers'
 export { default as AccordionExpandIcon } from './components/AccordionExpandIcon'
 export { useBreakpoints } from './hooks/useBreakpoints'
@@ -28,4 +37,13 @@ export type { TwakeTheme } from './lib/theme'
 export type { AvatarProps } from './components/Avatar'
 export type { ChipProps } from './components/Chip'
 export type { SidebarProps } from './components/Sidebar'
+export type {
+  NavProps,
+  NavItemProps,
+  NavLinkProps,
+  NavIconProps,
+  NavTextProps,
+  NavDesktopLimiterProps,
+  NavDesktopDropdownProps
+} from './components/Nav'
 export type { Breakpoints } from './hooks/useBreakpoints'
