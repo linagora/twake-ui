e4f9205162276b534ce888cf9d8de1a5b5d290c7
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-09-15 09:39:28 +0200

    refactor(twake-mui): Replace palette.mode check with theme.applyStyles in AppTitle
    We now could also use `useColorScheme()` to get the `mode` in JS
    if needed

--- packages/twake-mui/src/components/Apptitle/index.tsx
+++ packages/twake-mui/src/components/Apptitle/index.tsx
@@ -1,5 +1,6 @@
 import { Icon, TwakeText } from '@linagora/twake-icons'
-import { Box, useTheme } from '@mui/material'
+import { Box } from '@mui/material'
+import { CSSObject, Theme } from '@mui/material/styles'
 import React, { FC } from 'react'
 
 interface AppTitleProps {
@@ -8,9 +9,6 @@ interface AppTitleProps {
 }
 
 const AppTitle: FC<AppTitleProps> = ({ appIcon, appTextIcon }) => {
-  const theme = useTheme()
-  const isLight = theme.palette.mode === 'light'
-
   if (!appIcon || !appTextIcon) return null
 
   const AppTextIcon = appTextIcon
@@ -18,10 +16,23 @@ const AppTitle: FC<AppTitleProps> = ({ appIcon, appTextIcon }) => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
       <Icon icon={appIcon} size="32" />
-      <TwakeText height="22" style={{ fill: isLight ? '#000' : '#fff' }} />
-      <AppTextIcon
+      <Box
+        component={TwakeText}
+        height="22"
+        sx={[
+          { fill: '#000' },
+          (theme: Theme): CSSObject =>
+            theme.applyStyles('dark', { fill: '#fff' })
+        ]}
+      />
+      <Box
+        component={AppTextIcon}
         height={22}
-        style={{ width: 'auto', fill: isLight ? '#000' : '#fff' }}
+        sx={[
+          { width: 'auto', fill: '#000' },
+          (theme: Theme): CSSObject =>
+            theme.applyStyles('dark', { fill: '#fff' })
+        ]}
       />
     </Box>
   )
