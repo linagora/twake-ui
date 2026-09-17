986d9315bcc1d8a0c487125609cec7746ceccbf4
Théo Poizat <hello@zatteo.com>
2026-09-17 09:29:02 +0200

    chore(twake-mui): Rename the Selection story to Checkbox

--- packages/twake-mui/src/stories/Checkbox.stories.tsx
+++ packages/twake-mui/src/stories/Checkbox.stories.tsx
@@ -15,7 +15,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite'
 import React from 'react'
 
 const meta: Meta<typeof Checkbox> = {
-  title: 'Selection',
+  title: 'Checkbox',
   component: Checkbox,
   parameters: { layout: 'centered' },
   tags: ['autodocs'],
