28ed2c218849d75a8bbba62a0cf30d3d1884407d
Théo Poizat <hello@zatteo.com>
2026-09-21 13:46:23 +0200

    test(twake-mui): Update PlayWright setup to avoid flaky Argos tests
    See https://argos-ci.com/docs/learn/reliability-and-flakiness/flaky-tests/stabilize-text-rendering

--- packages/twake-mui/vitest.config.ts
+++ packages/twake-mui/vitest.config.ts
@@ -36,7 +36,11 @@ export default defineConfig({
           browser: {
             enabled: true,
             headless: true,
-            provider: playwright(),
+            provider: playwright({
+              launchOptions: {
+                args: ['--disable-lcd-text', '--font-render-hinting=none']
+              }
+            }),
             instances: [{ browser: 'chromium' }],
           },
         },
