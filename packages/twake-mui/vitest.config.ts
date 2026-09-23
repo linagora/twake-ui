f9fcf355238ce5f39fb090bdd34d7d40fc752d91
Théo Poizat <hello@zatteo.com>
2026-09-23 08:27:05 +0200

    test(twake-mui): Allow to have tests located next to code

--- packages/twake-mui/vitest.config.ts
+++ packages/twake-mui/vitest.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       {
         test: {
           name: 'unit',
-          include: ['test/**/*.test.ts'],
+          include: ['test/**/*.test.ts', 'src/**/*.test.ts'],
           environment: 'node',
         },
       },
