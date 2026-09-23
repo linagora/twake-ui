6a5263a2d5246798a1d003a8a2a7e0b5183bbe0d
JF-Cozy <julien.ficheux@cozycloud.cc>
2026-09-23 10:49:10 +0200

    feat(twake-mui): Migrate Tooltip from cozy-ui

--- packages/twake-mui/src/stories/Tooltip.stories.tsx
+++ packages/twake-mui/src/stories/Tooltip.stories.tsx
@@ -1,6 +1,24 @@
-import { Tooltip, Typography } from '@mui/material'
+import { CarbonCopy, Icon } from '@linagora/twake-icons'
+import { Stack, Tooltip, Typography } from '@mui/material'
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import React from 'react'
+
+const title = (
+  <>
+    <Stack direction="row" spacing={1} sx={{ alignItems: 'center', mb: 1 }}>
+      <Icon icon={CarbonCopy} />
+      <Typography variant="body1" color="inherit">
+        Carbon copy
+      </Typography>
+    </Stack>
+    <Typography variant="caption" color="inherit">
+      Indicates whether the document is defined as &ldquo;authentic and
+      original&rdquo; by Cozy Cloud, the host of your Cozy, as it can claim that
+      it comes directly from a third-party service, without having undergone any
+      modification.
+    </Typography>
+  </>
+)
 
 const meta: Meta<typeof Tooltip> = {
   title: 'Tooltip',
@@ -12,7 +30,7 @@ const meta: Meta<typeof Tooltip> = {
 export default meta
 type Story = StoryObj<typeof Tooltip>
 
-export const Docs: Story = {
+export const Default: Story = {
   argTypes: {
     placement: {
       control: 'select',
@@ -30,17 +48,7 @@ export const Docs: Story = {
     arrow: { control: 'boolean' }
   },
   args: {
-    title: (
-      <>
-        <Typography variant="body1" color="inherit">
-          Carbon copy
-        </Typography>
-        <Typography variant="caption" color="inherit">
-          Indicates whether the document is defined as &ldquo;authentic and
-          original&rdquo;
-        </Typography>
-      </>
-    ),
+    title,
     children: <u>hover over me</u>
   }
 }
@@ -48,21 +56,8 @@ export const Docs: Story = {
 export const Screenshot: Story = {
   tags: ['argos'],
   render: () => (
-    <div style={{ padding: 48 }}>
-      <Tooltip
-        open
-        title={
-          <>
-            <Typography variant="body1" color="inherit">
-              Carbon copy
-            </Typography>
-            <Typography variant="caption" color="inherit">
-              Indicates whether the document is defined as &ldquo;authentic and
-              original&rdquo;
-            </Typography>
-          </>
-        }
-      >
+    <div style={{ padding: '48px 48px 200px' }}>
+      <Tooltip open title={title}>
         <u>hover over me</u>
       </Tooltip>
     </div>
